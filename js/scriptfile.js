0e4df96d47d1ea26d6416dbe276780a6483a9a1e
salma <salmaessam6666@yahoo.com>
2021-01-04 19:58:28 +0200

    first commit

---
+++ js/scriptfile.js
@@ -0,0 +1,6 @@
+function sign(meh , nameWindo)
+            {
+                var x = screen.width/2 - 700/2;
+                var y = screen.height/2 - 450/2;
+                window.open(meh.href, nameWindo,'height=485,width=700,left='+x+',top='+y);
+            }
